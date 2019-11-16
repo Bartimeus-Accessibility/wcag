1f13e8cfa432cc47cc6ce14900eae84a5dfba807
Roel Antonisse <roel.antonisse@gmail.com>
2019-11-16 12:30:40 +0100

    Tweak aan wcag21 script om altId te gebruiken ipv id

--- script/wcag21.js
+++ script/wcag21.js
@@ -7,14 +7,17 @@ function linkUnderstanding() {
   if (respecConfig.specStatus == 'ED') understandingBaseURI = '../understanding/';
   else understandingBaseURI = 'https://www.w3.org/WAI/WCAG21/Understanding/';
   document.querySelectorAll('.sc').forEach(function(node){
-    var heading = node.firstElementChild.textContent;
-    var pathFrag = titleToPathFrag(node.id);
     var wcagVersionPath = node.classList.contains('new')
       ? '21/'
       : '20/';
+    var altId = node.dataset.altId || '';
+    var heading = node.firstElementChild.textContent;
+    var pathFrag = altId
+      ? wcagVersionPath + titleToPathFrag(altId)
+      : 'index';
     var el = document.createElement('div');
     el.setAttribute('class', 'doclinks');
-    el.innerHTML = '<a href="' + understandingBaseURI + wcagVersionPath + (pathFrag || 'index') + '.html">' + heading + ' begrijpen</a> <span class="screenreader">|</span> <br /><a href="https://www.w3.org/WAI/WCAG21/quickref/#' + pathFrag + '">Hoe te voldoen aan ' + heading + '</a>';
+    el.innerHTML = '<a href="' + understandingBaseURI + pathFrag   + '.html">' + heading + ' begrijpen</a> <span class="screenreader">|</span> <br /><a href="https://www.w3.org/WAI/WCAG21/quickref/#' + pathFrag + '">Hoe te voldoen aan ' + heading + '</a>';
     node.insertBefore(el, node.children[1]);
   });
 }
